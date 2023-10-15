cc274675353f2308d929bb9d067959c31ca94d37
Trương Xuân Nguyên <215748020110162@vinhuni.edu.vn>
2023-10-15 17:19:22 +0700

    15/10/2023

--- startup/Program.cs
+++ startup/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 var connection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connection));
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
